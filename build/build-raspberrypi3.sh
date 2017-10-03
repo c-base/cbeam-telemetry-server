db7bd87b3e00100648523fbc76c87325ff2eb6d1
Henri Bergius <henri.bergius@iki.fi>
2017-10-03 13:18:35 +0200

    Move travis_wait to .travis.yml

--- build/build-raspberrypi3.sh
+++ build/build-raspberrypi3.sh
@@ -2,4 +2,4 @@
 # prepare qemu for cross-compiling
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 # Build RPi3 image
-travis_wait 30 docker build -t cbase/cbeam-telemetry-server-raspberrypi3 -f Dockerfile-raspberrypi3 .
+docker build -t cbase/cbeam-telemetry-server-raspberrypi3 -f Dockerfile-raspberrypi3 .
