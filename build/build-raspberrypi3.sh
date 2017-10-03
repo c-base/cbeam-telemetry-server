6c910ce3602478ce8495a63c9d7f7af0dc6774a7
Henri Bergius <henri.bergius@iki.fi>
2017-10-03 14:19:55 +0200

    Use common convention for naming rpi images

--- build/build-raspberrypi3.sh
+++ build/build-raspberrypi3.sh
@@ -2,4 +2,4 @@
 # prepare qemu for cross-compiling
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 # Build RPi3 image
-docker build -t cbase/cbeam-telemetry-server-raspberrypi3 -f Dockerfile-raspberrypi3 .
+docker build -t $DOCKER_IMAGE -f Dockerfile-raspberrypi3 .
