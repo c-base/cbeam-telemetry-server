6aafddf202cc16966ea959e7fafbbc39ab2a0f49
Henri Bergius <henri.bergius@iki.fi>
2017-10-03 14:41:42 +0200

    Simplify Docker build

--- build/build-raspberrypi3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# prepare qemu for cross-compiling
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
-# Build RPi3 image
-docker build -t $DOCKER_IMAGE -f Dockerfile-raspberrypi3 .
--- build/build-x86.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Build x86 image
-docker build -t $DOCKER_IMAGE -f Dockerfile .
